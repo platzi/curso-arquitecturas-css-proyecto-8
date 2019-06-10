4382d1ec6238be8060763cfc684fd50f17294e70
Cristian Carvajal <carvagereal@gmail.com>
2019-06-10 09:37:23 -0500

    add twitter

--- src/App.vue
+++ src/App.vue
@@ -75,9 +75,11 @@ export default {
         [
             { id:1, icon : "fas fa-envelope", value :"cristian.carvajal@live.com" },
             { id:2, icon : "fab fa-linkedin", value :"linkedin.com/in/cricarba/" },
-            { id:3, icon : "fas fa-phone", value :"310 3358780" },
-            { id:4, icon : "fab fa-skype", value :"crisltree" },
-            { id:5, icon : "fas fa-map-marker", value :"Bogotá, CO" },
+             { id:5, icon : "fab fa-twitter", value :"@cricarba" },
+              { id:4, icon : "fas fa-phone", value :"310 3358780" },
+           
+            { id:6, icon : "fab fa-skype", value :"crisltree" },
+            { id:7, icon : "fas fa-map-marker", value :"Bogotá, CO" },
         ]
         }
   }
