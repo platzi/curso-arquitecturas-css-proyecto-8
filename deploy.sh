f472655ed7da1766b1092c086a6dfcebf267d0ee
cricarba <carvagereal@gmail.com>
2020-05-03 17:00:58 -0500

    commit

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'despliegue'
 #git push -f git@github.com:cricarba/cricarba.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:cricarba/profile.git master:gh-pages
+git push -f git@github.com:cricarba/profile.git profile2:gh-pages
 
 cd -
